e896ffab40c90d28b91a4e9b20ebb4c70e044cdd
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-23 00:22:08 -0300

    Desenvolvimento da aula 1.9

---
+++ Modulo-1/1.9-aula/comentarios.js
@@ -0,0 +1,20 @@
+/**
+ * arquivo: comentario.js
+ * descrição: explicação de como podemos usar comentários em JavaScript
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ * data: 22/12/2020
+ */
+
+// ==> Single Line Comments //
+let animal = 'cachorro';
+// console.log(animal);
+
+// ==> Multiple Line Comments /* */
+
+/* let num_1 = 10;
+let num_2 = 20;
+let num_3 = 30;*/
+let num_4 = 50;
+
+let somarTodos = num_4;
+console.log(somarTodos);
