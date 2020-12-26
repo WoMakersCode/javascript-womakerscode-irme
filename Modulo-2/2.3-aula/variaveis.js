0cd4067ae889f72c8825d05c19817469db646873
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-25 22:05:58 -0300

    Desenvolvimento da aula 2.3

---
+++ Modulo-2/2.3-aula/variaveis.js
@@ -0,0 +1,30 @@
+// Uso de variáveis em JavaScript
+
+// SOMA = num1 + num2
+
+/* var num1 = 10;
+var num2 = 20;
+
+var total = num1 + num2;
+console.log(total);*/
+
+// let
+/* let saudacao = "Olá, Programadoras!";
+
+if (true) {
+    let saudacao = "Quero aprender JavaScript";
+    console.log(saudacao);
+}
+
+console.log(saudacao);*/
+
+// const 
+const cidade = "Rio de Janeiro";
+
+function estado() {
+    const cidade = "São Paulo";
+    console.log(cidade);
+}
+
+estado(); // São Paulo
+console.log(cidade); // Rio de Janeiro
