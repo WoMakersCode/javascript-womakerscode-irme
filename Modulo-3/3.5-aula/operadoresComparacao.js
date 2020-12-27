7ace037f0dfc8ccddf45a58c0ab0dffc709d135d
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-27 20:07:38 -0300

    Desenvolvimento do exemplo 3.5

--- Modulo-3/3.5-aula/operadoresComparacao.js
+++ Modulo-3/3.5-aula/operadoresComparacao.js
@@ -3,8 +3,27 @@
  * descrição: explicação de como podemos usar operadores
  * de coparação em JavaScript
  * author: Glaucia Lemos - twitter: glaucia_lemos86
- * data: 26/12/2020
+ * data: 27/12/2020
  */
 
+// Igual a == (true ou false) - if
+/* console.log(10 == 10); // true -> verdadeiro
+console.log(12 == 10); // false -> falso */
 
+// Diferente de !=
+/* console.log(10 != 10); // false
+console.log(10 != 9); // true */
+
+// Maior que >
+/* console.log(10 > 9); // true
+console.log(9 > 10); // false
+ */
+
+// Menor que < 
+/* console.log(9 < 10); // true
+console.log(10 < 9);  // false */
+
+// Valor e tipo igual a === 
+console.log(10 === 10) // 10 ele é do tipo Number! (true)
+console.log(10 === 'Glaucia') // 10 -> Number | Glaucia -> String (false)
 
