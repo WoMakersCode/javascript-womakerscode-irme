fd96b51a754c420323260e63aa0346562509562b
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-27 19:53:05 -0300

    Desenvolvimento do exemplo 3.3

--- Modulo-3/3.3-aula/operadoresAritmeticos.js
+++ Modulo-3/3.3-aula/operadoresAritmeticos.js
@@ -3,6 +3,37 @@
  * descrição: explicação de como podemos usar operadores
  * aritméticos em JavaScript
  * author: Glaucia Lemos - twitter: glaucia_lemos86
- * data: 26/12/2020
+ * data: 27/12/2020
  */
 
+let num1 = 20;
+let num2 = 10;
+
+// Adição +
+
+let totalAdicao = num1 + num2;
+console.log(totalAdicao);
+
+// Subtração -
+let totalSubtracao = num1 - num2;
+console.log(totalSubtracao);
+
+// Multiplicação * 
+let totalMultiplicao = num1 * num2;
+console.log(totalMultiplicao);
+
+// Divisão / 
+let totalDivisão = num1 / num2;
+console.log(totalDivisão);
+
+// Resto % (mod)
+let restoDivisao = num1 % num2; // camel Case 
+console.log(restoDivisao);
+
+// Incremento ++ Loops for, do-do while
+console.log(num1++)
+console.log(++num1);
+
+// Decremento -- (Loops for, do-do while)
+console.log(num2--);
+console.log(--num2);
