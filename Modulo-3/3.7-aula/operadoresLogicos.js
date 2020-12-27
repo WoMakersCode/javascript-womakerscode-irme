516db78ba4ee0f7aac39797ac56ab34ed51e7b91
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-27 20:26:03 -0300

    Desenvolvimento do exemplo 3.7

--- Modulo-3/3.7-aula/operadoresLogicos.js
+++ Modulo-3/3.7-aula/operadoresLogicos.js
@@ -6,6 +6,23 @@
  * data: 26/12/2020
  */
 
+// Operador And && (tende a ser false)
+
+/* console.log(true && true); // true
+console.log(false && true); // false
+console.log(true && false); // false
+console.log(false && false); // false */
+
+// Operador Or || <- (pipe) (tende a ser true)
+
+/* console.log(true || true); // true
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(false || false); // false */
+
+// Operador Not !
+console.log(!true); // false
+console.log(!false); // true
 
 
 
