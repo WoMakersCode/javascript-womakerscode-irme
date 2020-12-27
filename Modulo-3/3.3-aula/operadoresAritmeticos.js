f680718f1e523ed08cf7f58cfc90ac28474a404a
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-26 23:05:04 -0300

    Criando os exemplos da aula 3.3

---
+++ Modulo-3/3.3-aula/operadoresAritmeticos.js
@@ -0,0 +1,7 @@
+/**
+ * arquivo: operadoresAritmeticos.js
+ * descrição: explicação de como podemos usar operadores
+ * aritméticos em JavaScript
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ * data: 26/12/2020
+ */
