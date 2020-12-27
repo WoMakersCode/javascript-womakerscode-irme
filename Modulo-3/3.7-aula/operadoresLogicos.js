85b83bce536bca1984b8f949e47fb1d123b88862
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-27 00:51:38 -0300

    Inclusão do arquivo 3.7

---
+++ Modulo-3/3.7-aula/operadoresLogicos.js
@@ -0,0 +1,12 @@
+/**
+ * arquivo: operadoresLogicos.js
+ * descrição: explicação de como podemos usar operadores
+ * lógicos em JavaScript
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ * data: 26/12/2020
+ */
+
+
+
+
+
