97446144bd81d9c012a881f0cb5a8c18524f76d7
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-29 00:20:49 -0300

    Criando a estrutura para o módulo 4

---
+++ Modulo-4/4.3-aula/condicional.js
@@ -0,0 +1,12 @@
+/**
+ * arquivo: condicional.js
+ * descrição: explicação de como podemos usar if e if-else
+ * author: Glaucia Lemos - twitter: glaucia_lemos86
+ * data: 28/12/2020
+ */
+
+// Condicional if & if-else
+
+
+
+// Condicional operador (ternario)
