35db86d1c29b062cfa35ed6e938a59d6085ec487
glaucia86 <glaucia_lemos86@hotmail.com>
2020-12-30 00:56:06 -0300

    Desenvolvimento do ex 4.3

--- Modulo-4/4.3-aula/condicional.js
+++ Modulo-4/4.3-aula/condicional.js
@@ -2,11 +2,28 @@
  * arquivo: condicional.js
  * descrição: explicação de como podemos usar if e if-else
  * author: Glaucia Lemos - twitter: glaucia_lemos86
- * data: 28/12/2020
+ * data: 30/12/2020
  */
 
-// Condicional if & if-else
+// Condicional if 
 
+/* let podeDirigir = 16;
 
+if (podeDirigir >= 18) {
+    console.log('Tem permissão para dirigir!');
+} */
+
+// Condicional if-else
+/* let podeDirigir = 16;
+
+if (podeDirigir >= 18) {
+    console.log('Tem permissão para dirigir!');
+} else {
+    console.log('Você não tem permissão para dirigir!');
+} */
 
 // Condicional operador (ternario)
+let idade = 16;
+let podeDirigir = idade >= 18 
+    ? console.log('Tem permissão para dirigir!') 
+    : console.log('Você não tem permissão para dirigir');
