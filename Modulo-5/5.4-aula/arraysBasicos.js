04d6233e227220e49c6bf1d22945427c431cd0e8
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-09 19:35:13 -0300

    Desenvolvimento do ex.: 5.4

--- Modulo-5/5.4-aula/arraysBasicos.js
+++ Modulo-5/5.4-aula/arraysBasicos.js
@@ -7,10 +7,52 @@
 
 // ==> pop();
 
+/* let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+
+console.log(eletrodomesticos.pop());
+ */
+
 // ==> push();
+
+/* let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+console.log('Sem o push..: ', eletrodomesticos);
+console.log(eletrodomesticos.push('Cafeteira'));
+console.log('Com o push...: ',  eletrodomesticos); */
 
 // ==> shift();
 
+/* let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+
+console.log('Sem o shift..: ', eletrodomesticos);
+console.log(eletrodomesticos.shift());
+console.log('Com o shift...: ',  eletrodomesticos);  */
+
 // ==> unshift();
+let eletrodomesticos = [
+    'Televisão',
+    'Geladeira',
+    'Fogão',
+    'Microondas' 
+];
+
+console.log('Sem o unshift..: ', eletrodomesticos);
+console.log(eletrodomesticos.unshift('Batedeira'));
+console.log('Com o unshift...: ',  eletrodomesticos);
+
 
 
