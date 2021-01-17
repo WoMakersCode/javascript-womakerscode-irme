890d35f19cac194c348e52d951fde73ca6a3fcf8
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-16 21:57:36 -0300

    Adicionando arquivos para iniciar as demos.

--- Modulo-6/6.6-aula/objetosJavaScript.js
+++ Modulo-6/6.6-aula/objetosJavaScript.js
@@ -5,3 +5,15 @@
  * data: 15/01/2021
  */
 
+/* let variavelGlobal = 'Oi! Eu sou uma variável Global!';
+
+function novoEscopo(){
+    let funcaoVariavelEscopo = 'Eu moro no escopo da função novoEscopo';
+}
+
+variavelGlobal; // "Oi! Eu sou uma variável Global!";
+novoEscopo(); 
+
+funcaoVariavelEscopo; 
+ */
+
