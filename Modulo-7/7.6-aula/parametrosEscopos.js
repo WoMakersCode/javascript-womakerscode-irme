0b563673fe22bbb23eeac6f159b06784153ff621
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-18 20:46:54 -0300

    exemplo 7.6

--- Modulo-7/7.6-aula/parametrosEscopos.js
+++ Modulo-7/7.6-aula/parametrosEscopos.js
@@ -6,5 +6,24 @@
  * data: 18/01/2021
  */
 
+// ==> Function -> Parâmetros
 
+/* function adicionarNumeros(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+let resultadoFinal = adicionarNumeros(5,5);
+console.log(resultadoFinal); */
+
+// ==> Escopo Global
+
+let localNascimento = 'Rio de Janeiro';
+
+function mostrarLocalNascimento() {
+    let localOndeMorei = 'Buenos Aires';
+};
+
+console.log(localNascimento);
+
+mostrarLocalNascimento();
 
