e08571f12e362f5b7ca374f3624220b9f866d07f
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-18 20:09:43 -0300

    Aula 7.4

--- Modulo-7/7.4-aula/funcoes.js
+++ Modulo-7/7.4-aula/funcoes.js
@@ -6,5 +6,34 @@
  */
 
 
+// ==> Função Anônima
 
+// ==> Exemplo 1
+/* function mostrarMensagem() {
+    return console.log('Parabéns! Você chegou ao final do treinamento de JavaScript');
+}
 
+mostrarMensagem(); */
+
+// ==> Exemplo 2
+
+/* function multiplarNumeros(numero1, numero2) {
+    return numero1 * numero2;
+}
+
+let resultadoFinal = multiplarNumeros(3,2);
+console.log(resultadoFinal); */
+
+// ==> Exemplo 3
+
+let numeros = [1,2,3,4,5,6,7,8,9,10];
+
+function mostrarNumerosImpares() {
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 !== 0) {
+            console.log(numeros[i]);
+        }
+    }
+}
+
+mostrarNumerosImpares();
