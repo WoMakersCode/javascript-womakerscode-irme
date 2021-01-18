0a11c9ea502a79c498eaadcb1a033e8dbb102825
glaucia86 <glaucia_lemos86@hotmail.com>
2021-01-18 17:33:45 -0300

    Adicionando o exemplo 6.9

--- Modulo-6/6.9-aula/iteracaoObjetos.js
+++ Modulo-6/6.9-aula/iteracaoObjetos.js
@@ -3,12 +3,36 @@
  * descrição: explicação de como podemos usar Interações de Objetos
  *  em JavaScript
  * author: Glaucia Lemos - twitter: glaucia_lemos86
- * data: 15/01/2021
+ * data: 18/01/2021
  */
 
-let funcionario = {
-    754: 'Renato',
-    843: 'Alice',
-    921: 'Paula',
-    192: 'Daniel'
+// ==> For... in
+
+/* funcionario = {
+    nome: 'Glaucia',
+    sobrenome: 'Lemos',
+    profissao: 'Cloud Advocate',
+    idade: 34,
+    empresa: 'Microsoft',
+    ativa: true
 };
+
+for (let listarFuncionario in funcionario) {
+    console.log(funcionario[listarFuncionario]);
+}
+ */
+
+// ==> If... in
+
+funcionario = {
+    nome: 'Glaucia',
+    sobrenome: 'Lemos',
+    profissao: 'Cloud Advocate',
+    idade: 34,
+    empresa: 'Microsoft',
+    ativa: true
+};
+
+if ('profissao' in funcionario) {
+    console.log('A key profissão existe no objeto Funcionario');
+}
